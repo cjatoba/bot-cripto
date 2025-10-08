7c0874743ea69f67444aafb5d8f0b28d087d1db5
cjatoba <claytonjatoba@gmail.com>
2025-10-08 08:12:03 -0300

    feat: get price and start buy and sell logic

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 import axios from "axios";
 
-const SYMBOL = "BTCUSD";
+const SYMBOL = "BTCUSDT";
 const BUY_PRICE = 120672;
 const SELL_PRICE = 122121;
 
-const API_URL = "https://testnet.binance.vision";
+const API_URL = "https://testnet.binance.vision"; //https://api.binance.com
+
+let isOpened = false;
 
 async function start() {
     const {data} = await axios.get(`${API_URL}/api/v3/klines?limit=21&interval=15m&symbol=${SYMBOL}`)
     const candle = data[data.length - 1];
+    //[1]open
+    //[2]high
+    //[3]low
+    //[4]close
+    const price = parseFloat(candle[4]);
 
-    console.log(candle);
+    console.clear()
+    console.log(price);
+
+    if (price <= BUY_PRICE && !isOpened) {
+        console.log("buy")
+
+        isOpened = true;
+    }
+    else if (price >= SELL_PRICE && isOpened) {
+        console.log("sell")
+
+        isOpened = false;
+    }
+    else
+        console.log("wait")
 }
 
 setInterval(() => {
